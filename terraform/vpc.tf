f1d688683af3b353f707079a0168e1d798d46223
chetantalele <chetantalele06@gmail.com>
2026-01-18 19:09:58 +0530

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,6 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.cluster_name}-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -10,4 +14,12 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.${count.index}.0/24"
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.cluster_name}-public-${count.index}"
+
+    # 🔑 REQUIRED for EKS + ALB Controller
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
 }
